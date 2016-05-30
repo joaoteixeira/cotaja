09fdfa05375b075164202f16e5fe502c4a394b42
Joao Teixeira <joaotexjunior@gmail.com>
2016-05-29 22:51:41 -0400

    ajustes

--- config/view.php
+++ config/view.php
@@ -2,7 +2,6 @@
 
 return [
 
-    'error' => 'errors.exception',
     /*
     |--------------------------------------------------------------------------
     | View Storage Paths
@@ -23,7 +22,6 @@ return [
     | Compiled View Path
     |--------------------------------------------------------------------------
     |
-
     | This option determines where all the compiled Blade templates will be
     | stored for your application. Typically, this is within the storage
     | directory. However, as usual, you are free to change this value.
